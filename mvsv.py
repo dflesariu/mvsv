f847ab8f933f0c16530915485ee897ee9b89fd4a
dflesariu <flesariudragos@gmail.com>
2025-02-10 00:31:42 +0000

    initial draft

--- mvsv.py
+++ mvsv.py
@@ -1,11 +1,10 @@
 import subprocess
 
-input_folder = "/Users/d/Documents/code/mvsv"
+input_folder = "/Users/{username}/Documents/code/mvsv"
 movies_folder = ""
 series_folder = ""
 
 #list all file names, A=not implied . and .., 1= only one name per line
-#listing = subprocess.Popen(["ls", "-A1", input_folder], stdout=subprocess.PIPE)
-listing = subprocess.check_output(["ls", "-A1", input_folder])
+listing = subprocess.check_output(['ls', '-A1', input_folder])
 
 print (listing)
